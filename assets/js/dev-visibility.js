22bb5da69933cd79b6cc5ca5eb0f13e1514e3693
Ivopc <ivoopc@gmail.com>
2024-01-15 06:37:47 -0300

    multilang in dev infos visibility added

--- assets/js/dev-visibility.js
+++ assets/js/dev-visibility.js
@@ -15,7 +15,7 @@ async function showDevVisibility({title, confirmButtonText, denyButtonText, html
 const showDevVisibilityLangs = {
     "BR": {
         title: 
-        `Você é alguém ligado a desenvolvimento? Para deixar meu site mais resumido para quem não entende de programação
+        `Para deixar meu site mais resumido para quem não entende de programação
         você tem a opção de ocultar as partes mais técnicas que falam somente sobre programação. Lembrando que é possível controlar no menu de navegação.`,
         confirmButtonText: "Sim, desejo ocultar!",
         denyButtonText: "Naõ, pode manter!",
@@ -23,6 +23,19 @@ const showDevVisibilityLangs = {
         <input class="form-check-input" type="checkbox" value="" id="hideDevVisibilityCheck" checked>
         <label class="form-check-label" for="hideDevVisibilityCheck">
           Naõ mostrar isso novamente
+        </label>
+        `
+    },
+    "EN": {
+        title: 
+        `To make my website more concise for those who don't understand programming,
+        you have the option to hide the more technical parts that only talk about programming. Remember that you can control this in the navigation menu.`,
+        confirmButtonText: "Yes, I want to hide!",
+        denyButtonText: "No, you can keep it!",
+        html: `
+        <input class="form-check-input" type="checkbox" value="" id="hideDevVisibilityCheck" checked>
+        <label class="form-check-label" for="hideDevVisibilityCheck">
+          Do not show this again
         </label>
         `
     }
