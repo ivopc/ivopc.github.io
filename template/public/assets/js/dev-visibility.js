6d41deb807c3567b91c1af1fe760d69b7041da4e
Ivopc <ivoopc@gmail.com>
2024-01-17 18:21:45 -0300

    hugo template fully done

---
+++ template/public/assets/js/dev-visibility.js
@@ -0,0 +1,42 @@
+async function showDevVisibility({title, confirmButtonText, denyButtonText, html}) {
+    const showDevVisibility = await Swal.fire({
+        title,
+        html,
+        showDenyButton: true,
+        confirmButtonText,
+        denyButtonText
+    });
+    localStorage.setItem("hideDevVisibilityCheck", document.querySelector("#hideDevVisibilityCheck").checked ? "true" : "false")
+    if (showDevVisibility.isConfirmed) {
+        hideDevInfo ();
+    };
+};
+
+const showDevVisibilityLangs = {
+    "BR": {
+        title: 
+        `Para deixar meu site mais resumido para quem não entende de programação
+        você tem a opção de ocultar as partes mais técnicas que falam somente sobre programação. Lembrando que é possível controlar no menu de navegação.`,
+        confirmButtonText: "Sim, desejo ocultar!",
+        denyButtonText: "Naõ, pode manter!",
+        html: `
+        <input class="form-check-input" type="checkbox" value="" id="hideDevVisibilityCheck" checked>
+        <label class="form-check-label" for="hideDevVisibilityCheck">
+          Naõ mostrar isso novamente
+        </label>
+        `
+    },
+    "EN": {
+        title: 
+        `To make my website more concise for those who don't understand programming,
+        you have the option to hide the more technical parts that only talk about programming. Remember that you can control this in the navigation menu.`,
+        confirmButtonText: "Yes, I want to hide!",
+        denyButtonText: "No, you can keep it!",
+        html: `
+        <input class="form-check-input" type="checkbox" value="" id="hideDevVisibilityCheck" checked>
+        <label class="form-check-label" for="hideDevVisibilityCheck">
+          Do not show this again
+        </label>
+        `
+    }
+};
